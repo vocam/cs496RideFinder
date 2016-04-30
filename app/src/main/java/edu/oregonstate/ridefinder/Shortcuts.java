fe38abf9929a1a5960140ca5aa43fc22dc0b61ef
Cam Tu Vo <camtuvo@Cams-MBP.hsd1.or.comcast.net>
2016-04-30 15:23:27 -0700

    Initial commit

---
+++ app/src/main/java/edu/oregonstate/ridefinder/Shortcuts.java
@@ -0,0 +1,10 @@
+package edu.oregonstate.ridefinder;
+
+/**
+ * Created by camtuvo on 4/29/16.
+ */
+public class Shortcuts {
+    /* Making some constants to avoid lengthy typing */
+    public static final String FIREBASE_REPO = "ridefinder1";
+    public static final String FIREBASE_URL = "https://" + FIREBASE_REPO + "firebaseIO.com/";
+}
